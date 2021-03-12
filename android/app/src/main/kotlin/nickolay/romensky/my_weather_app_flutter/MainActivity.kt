ffafac350c5c8f8faa5b2403c772b0444c64f57d
user <nromensky@gmail.com>
2021-03-12 18:25:22 +0300

    .

---
+++ android/app/src/main/kotlin/nickolay/romensky/my_weather_app_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package nickolay.romensky.my_weather_app_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyWeatherApp());
+}
+
+class MyWeatherApp extends StatefulWidget {
+  @override
+  _MyWeatherAppState createState() => _MyWeatherAppState();
+}
+
+class _MyWeatherAppState extends State<MyWeatherApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
